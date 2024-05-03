8a8ad7a7d01ff81ad28dbaf03bb5c242084106b1
kenandjak <100363820+kenandjak@users.noreply.github.com>
2024-05-03 14:02:37 -0300

    Redimensionamento das imagens
    Alteração das dimensões das imagens e sincronização com o horário do dia.

--- site_horarios/script.js
+++ site_horarios/script.js
@@ -1,22 +1,23 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora>=0 && hora < 12){
-        //Bom dia
-        img.scr = 'morning.jpg'
-        document.body.style.background = '#ebe400'
-    } else if (hora >= 12 && hora < 18){
-        //Boa tarde
-        img.scr = 'afternoon.jpg'
-        document.body.style.background = '#0ec40e'
-    } else{
-        //Boa noite
-        img.scr = 'night.jpg'
-        document.body.style.background = '#1c13c2'
-    }
+function carregar() {
+  var msg = window.document.getElementById("msg")
+  var img = window.document.getElementById("imagem")
+  var data = new Date()
+  var hora = data.getHours()
+  //var hora = 20
+  msg.innerHTML = `Faixa de horas: Agora são ${hora}h.`
+  if (hora >= 0 && hora < 12) {
+    //Bom dia
+    img.src = 'morning1.jpg'
+    document.body.style.background = "#ebe400"
+  } else if (hora >= 12 && hora < 18) {
+    //Boa tarde
+    img.src = 'afternoon1.jpg'
+    document.body.style.background = "#0ec40e"
+  } else {
+    //Boa noite
+    img.src = 'night1.jpg'
+    document.body.style.background = "#1c13c2"
+  }
 }
 
 //scr="./LEARNING_JAVASCRIPT/site_horarios/morning.jpg"
